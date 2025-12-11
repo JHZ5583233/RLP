d1f7da0a8fd8171025c757a5cad6f1ff9c7fa701
JHZ5583233 <j.h.zhang@student.rug.nl>
2025-12-11 15:17:17 +0100

    now proper installed

---
+++ agent/agent.py
@@ -0,0 +1,13 @@
+import gymnasium as gym
+import numpy as np
+
+from stable_baselines3 import DDPG
+from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
