dedd63f40aa440c594b9d3b0a2051f0d19c89ef0
JHZ5583233 <j.h.zhang@student.rug.nl>
2025-12-11 15:41:06 +0100

    start of hyper param

--- agent/agent.py
+++ agent/agent.py
@@ -6,7 +6,16 @@ from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckA
 
 
 def main():
-    pass
+    env = gym.make('Swimmer-v5', render_mode="rgb_array")
+
+    # The noise objects for DDPG
+    n_actions = env.action_space.shape[-1]
+    action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
+
+    model = DDPG("MlpPolicy", env, action_noise=action_noise, verbose=1)
+    model.learn(total_timesteps=10000, log_interval=10)
+    model.save("ddpg_yes")
+    vec_env = model.get_env()
 
 
 if __name__ == '__main__':
